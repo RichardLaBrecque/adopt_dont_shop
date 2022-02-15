1e940308b22b3bad86fc52434a73fe4c45d8e57a
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-14 20:52:10 -0500

    fix: change link_to into button_to

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
-  get '/applications/:application_id/:pet_id/petapplications', to: 'pet_applications#create' #not restful, change to button/post
+  post '/applications/:application_id/:pet_id/petapplications', to: 'pet_applications#create' #not restful, change to button/post
   get '/applications/new', to: 'applications#new'
   get '/applications', to: 'applications#index'
   post '/applications', to: 'applications#create'
